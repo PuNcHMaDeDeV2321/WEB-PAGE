ffee8806b374d4700c6c30739521f31fd574a2f1
PuNcHMaDeDeV2321 <160447914+PuNcHMaDeDeV2321@users.noreply.github.com>
2024-02-26 01:53:44 -0300

    Update

---
+++ COOKIES TO JSON/script.js
@@ -0,0 +1,30 @@
+function convertirCookies() {
+    var cookiesNetscape = document.getElementById("cookie").value.trim();
+    var cookiesArray = cookiesNetscape.split('\n');
+
+    var cookiesJson = [];
+    cookiesArray.forEach(function (cookie) {
+        var cookieParts = cookie.split('\t');
+        var cookieObject = {
+            "domain": cookieParts[0],
+            "path": cookieParts[2],
+            "name": cookieParts[5],
+            "value": cookieParts[6]
+        };
+        cookiesJson.push(cookieObject);
+    });
+
+    var resultadoJson = JSON.stringify(cookiesJson, null, 4);
+    document.getElementById("result").value = resultadoJson;
+}
+
+function changeMode() {
+    var cuerpo = document.body;
+    if (cuerpo.classList.contains("modo-noche")) {
+        cuerpo.classList.remove("modo-noche");
+        cuerpo.classList.add("modo-claro");
+    } else {
+        cuerpo.classList.remove("modo-claro");
+        cuerpo.classList.add("modo-noche");
+    }
+}
